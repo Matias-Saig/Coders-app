2f1d89bbe258ee81382a3ab068bece8cddc55eb7
matias-saig <saig.matias@gmail.com>
2024-03-14 11:13:40 -0300

    entrega-seis

--- screens/SingUp.jsx
+++ screens/SingUp.jsx
@@ -8,6 +8,7 @@ import ErrorMsg from "../components/Elements/ErrorMsg";
 import FormButton from "../components/Elements/FormButton";
 import FormContainer from "../components/Elements/FormContainer";
 import { Alert } from "react-native";
+import LoadingMsg from "../components/Elements/LoadingMsg";
 
 const SingUp = ({ navigation }) => {
   const dispatch = useDispatch();
@@ -21,9 +22,13 @@ const SingUp = ({ navigation }) => {
 
   const [triggerRegister] = useSignUpMutation();
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [loginError, setLoginError] = useState("");
+
   const onSubmit = async () => {
     try {
-      registerSchema.validateSync({email,password,confirmPassword})
+      registerSchema.validateSync({ email, password, confirmPassword });
       const { data } = await triggerRegister({ email, password });
       dispatch(
         setUser({
@@ -51,6 +56,13 @@ const SingUp = ({ navigation }) => {
         default:
           break;
       }
+      if (error.data?.error) {
+        setLoginError(error.data.error);
+      } else {
+        setLoginError("El usuario o la contraseña no son correctos");
+      }
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -78,6 +90,8 @@ const SingUp = ({ navigation }) => {
       <ErrorMsg error={errorConfirmPassword} />
 
       <FormButton fx={onSubmit} text="Registrarme" />
+      {isLoading && <LoadingMsg text="Cargando datos" />}
+      {loginError && <ErrorMsg error={loginError} />}
     </FormContainer>
   );
 };
