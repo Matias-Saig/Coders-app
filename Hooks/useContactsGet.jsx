c26e202dde40e4774d9d785c5ecd2f9a31ae48b8
Matias Saig <saig.matias@gmail.com>
2024-04-08 16:27:22 -0300

    comments

--- Hooks/useContactsGet.jsx
+++ Hooks/useContactsGet.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useGetContactsQuery } from "../app/Service/userContactsApi";
 
+// get contact list
 function useContactsGet() {
   const user = useSelector((state) => state.auth);
   const { data, isLoading, isError, error, isFetching } = useGetContactsQuery(
